826c81d3669338ae46275d4b4fb4ba85f45670e6
Lapo0 <lapo.gini.01@gmail.com>
2023-02-21 15:18:44 +0100

    FEAT BONUS Enter addTask in input

---
+++ BONUS/js/app.js
@@ -0,0 +1,44 @@
+const { createApp } = Vue
+
+  createApp({
+    data() {
+      return {
+        todoList: [
+            {
+            text: 'Bere tanto',
+            done: true,
+            },
+            {
+            text: 'Ricordarsi di bere tanto',
+            done: false,
+            },
+            {
+            text: 'Ricordarsi di ricordarsi di bere tanto',
+            done: true,
+            },
+            {
+            text: 'Ricordarsi di bere acqua',
+            done: false,
+            },
+            {
+            text: 'Comprare il Gin',
+            done: false,
+            },
+        ],
+        newTask: ''
+      }
+    },
+    methods: {
+        removeTask(taskIndex) {
+            this.todoList.splice(taskIndex, 1)
+        },
+        addTask() {
+        this.todoList.push({
+            text: this.newTask,
+            done: false
+        });
+        this.newTask = '';
+        }
+    }
+    
+  }).mount('#app')
